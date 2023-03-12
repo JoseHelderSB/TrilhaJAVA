fef49dcbda8ae07ab505359378ecfda72ee88ac4
Jose Helder <joseheldersb@gmail.com>
2023-03-12 18:31:26 -0300

    exercicio 3

---
+++ src/loops/ex3_MaiorEMedia.java
@@ -0,0 +1,31 @@
+package loops;
+import java.util.Scanner;
+/*Programa que leia 5 números
+ e informa o maior número e a média deles
+ */
+
+public class ex3_MaiorEMedia {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+        int numero;
+        int count = 0;
+        int maior = 0;
+        int soma = 0;
+        double media = 0;
+
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+            soma = soma + numero;
+            if(numero > maior) maior = numero;
+            count++;
+        }while (count < 5);
+
+        System.out.println("O maior número é: " + maior);
+        System.out.println("A média dos números é: " + (soma / count));
+
+
+
+    }
+    
+}
